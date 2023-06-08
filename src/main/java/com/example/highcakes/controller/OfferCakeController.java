7923662fe031978174419a3c022c993a1fbd5813
XeindsTwo <yourihelo1971@gmail.com>
2023-06-08 19:07:10 +0300

    work

--- src/main/java/com/example/highcakes/controller/OfferCakeController.java
+++ src/main/java/com/example/highcakes/controller/OfferCakeController.java
@@ -4,11 +4,14 @@ import com.example.highcakes.impl.OfferImpl;
 import com.example.highcakes.model.Cake;
 import com.example.highcakes.model.Offer;
 import com.example.highcakes.repo.CakeRepo;
+import com.example.highcakes.repo.EmailService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -17,10 +20,12 @@ public class OfferCakeController {
 
     private final CakeRepo cakeRepo;
     private final OfferImpl offerImpl;
+    private final EmailService emailService;
 
-    public OfferCakeController(CakeRepo cakeRepo, OfferImpl offerImpl) {
+    public OfferCakeController(CakeRepo cakeRepo, OfferImpl offerImpl, EmailService emailService) {
         this.cakeRepo = cakeRepo;
         this.offerImpl = offerImpl;
+        this.emailService = emailService;
     }
 
     @GetMapping("/offer")
@@ -36,7 +41,21 @@ public class OfferCakeController {
     @PostMapping("/offer/save")
     public String createOffer(@ModelAttribute("offer") Offer offer) {
         offerImpl.save(offer);
+        String to = offer.getEmail();
+        String subject = "Заявка на заказ торта";
+        String text = "Была создана новая заявка: " + offer.toString();
+        emailService.sendEmail(to, subject, text);
+
         System.out.print("Добавлена заявка!");
+        return "redirect:/offer";
+    }
+
+    @GetMapping("/offer/delete/{id}")
+    public String delete(@PathVariable Long id, RedirectAttributes redirectAttributes) {
+        Offer offer = offerImpl.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid offer Id:" + id));
+        offerImpl.delete(offer.getId());
+        redirectAttributes.addFlashAttribute("success", "Успешное удаление заявки " + offer.getName());
         return "redirect:/offer";
     }
 }
---
+++ src/main/resources/static/js/offer.js
@@ -0,0 +1,24 @@
+const modalDelete = document.getElementById('modal-delete_offer');
+const body = document.body;
+let curr_offer_id = -1;
+
+function modal_DeleteOffer() {
+    console.log("open")
+    let offer_id = $(event.target).closest("button").attr("data-offer-id");
+    let offer_name = $(event.target).closest("button").attr("data-offer-name");
+    body.classList.toggle('body--active');
+    modalDelete.classList.toggle('modal--active');
+    curr_offer_id = offer_id;
+    $("#offer_name-delete").html(offer_name);
+}
+
+function modal_DeleteOfferClose() {
+    console.log("close")
+    body.classList.remove('body--active');
+    modalDelete.classList.remove('modal--active');
+    curr_offer_id = -1;
+}
+
+function deleteOffer() {
+    window.location.href = `offer/delete/${curr_offer_id}`;
+}
