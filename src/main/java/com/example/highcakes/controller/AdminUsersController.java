0d768fa6beb63a75d1b48eeeaf909977179e2d2d
XeindsTwo <125365638+XeindsTwo@users.noreply.github.com>
2023-06-16 23:42:47 +0300

    work

---
+++ src/main/java/com/example/highcakes/controller/AdminUsersController.java
@@ -0,0 +1,22 @@
+package com.example.highcakes.controller;
+
+import com.example.highcakes.impl.UserImpl;
+import com.example.highcakes.model.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class AdminUsersController {
+    private final UserImpl userImpl;
+    @GetMapping("/users")
+    public String usersPage(Model model) {
+        List<User> users = userImpl.findAll();
+        model.addAttribute("users", users);
+        return "users";
+    }
+}
