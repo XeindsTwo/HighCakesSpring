19ef0f26c3fce2ab5024e3fa5af071a532067857
XeindsTwo <125365638+XeindsTwo@users.noreply.github.com>
2023-06-18 17:36:13 +0300

    work

--- src/main/java/com/example/highcakes/controller/AdminUsersController.java
+++ src/main/java/com/example/highcakes/controller/AdminUsersController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -27,14 +28,12 @@ public class AdminUsersController {
     @GetMapping("/users")
     public String usersPage(Model model, @RequestParam(value = "param", defaultValue = "", required = false) String param) {
         List<User> users;
-        List<Role> roles = Arrays.asList(Role.values());
         if (param != null && !param.isEmpty()) {
             users = userRepo.findByUsernameContainingIgnoreCaseOrNameContainingIgnoreCaseOrNumberContainingIgnoreCaseOrMailContainingIgnoreCase(param, param, param, param);
             model.addAttribute("param", param);
         } else {
             users = userRepo.findAll();
         }
-        model.addAttribute("roles", roles);
         model.addAttribute("users", users);
         return "users";
     }
@@ -46,5 +45,22 @@ public class AdminUsersController {
         userImpl.delete(user.getId());
         redirectAttributes.addFlashAttribute("success", "Успешное удаление пользователя " + user.getName());
         return "redirect:/users";
+    }
+
+    @GetMapping("users/save")
+    public String save(Model model) {
+        List<Role> roles = Arrays.asList(Role.values());
+        model.addAttribute("roles", roles);
+        return "add-user";
+    }
+
+    @GetMapping("users/{id}/edit")
+    public String edit(@PathVariable("id") Long id, Model model) {
+        User user = userRepo.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid cake Id:" + id));
+        List<Role> roles = Arrays.asList(Role.values());
+        model.addAttribute("roles", roles);
+        model.addAttribute("user", user);
+        return "edit-user";
     }
 }
